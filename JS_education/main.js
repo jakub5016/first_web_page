5552d7c96a384b39a42b8c82faea674383a2bde8
Jaqobel <me.jakub.gil@gmail.com>
2023-07-11 22:53:23 +0200

    Continue of app in JS

--- JS_education/main.js
+++ JS_education/main.js
@@ -14,4 +14,19 @@ const deposit = () => {
     }
 }
 
-const depositAmout = deposit();
+const getNumberOfLines = () =>{
+    while (true){
+    const lines = prompt("Enter a number of lines (1-3): ");
+    const numberOfLines =parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <=0 || numberOfLines >3){
+            console.log("INVALID NUMBER OF LINES");
+        }
+        else {
+            return numberOfLines;
+        }
+    }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
